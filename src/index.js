96c15edd988a450f74e76838fc6b0c87d8938374
luongdat2k4 <luongdat808@gmail.com>
2025-02-15 21:00:53 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const accordion = document.querySelector(".accordion");
+const isRotate = document.querySelector(".bx-chevron-up");
+
+accordion.addEventListener("click", (e) => {
+  const activePanel = e.target.closest(".accordion-panel");
+  if (!activePanel) return;
+  toggleAccordion(activePanel);
+});
+
+
+function toggleAccordion(panelToActivate) {
+  const activeButton = panelToActivate.querySelector("button");
+  const activePanel = panelToActivate.querySelector(".accordion-content");
+  const activePanelIsOpened = activeButton.getAttribute("aria-expanded");
+
+  if (activePanelIsOpened === "true") {
+    isRotate.classList.remove("rotate");
+
+    panelToActivate
+      .querySelector("button")
+      .setAttribute("aria-expanded", false);
+
+    panelToActivate
+      .querySelector(".accordion-content")
+      .setAttribute("aria-hidden", true);
+
+  } else {
+    isRotate.classList.add("rotate");
+
+    panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
+
+    panelToActivate
+      .querySelector(".accordion-content")
+      .setAttribute("aria-hidden", false);
+  }
+}
